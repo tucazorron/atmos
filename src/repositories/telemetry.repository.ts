bb200cc4b3bb908ab798b30824c1e67fdeca5e47
Mariana Jube <marianaramosjube@gmail.com>
2023-11-02 15:38:50 -0300

    changes repository

--- src/repositories/telemetry.repository.ts
+++ src/repositories/telemetry.repository.ts
@@ -1,11 +1,12 @@
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Telemetry } from 'src/entities/telemetry.entity';
+import { Repository } from 'typeorm';
 
 export class TelemetryRepository {
   static async createTelemetry(topic: string, message: Buffer) {
     const payload = JSON.parse(message.toString());
     const telemetry = new Telemetry(payload);
     telemetry.mac = topic;
-    await getRepositoryToken(Telemetry).save(telemetry);
+    await Repository<Telemetry>.save(telemetry);
   }
 }
